f04eb52e75ab1c1f316a5fa74d01cf67c6c6c253
Alrocha2301 <alexandre-apr@hotmail.com>
2022-10-13 15:44:53 -0300

    Initial commit

---
+++ src/test/java/com/alexandre/estacionamentonuvem/EstacionamentoNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandre.estacionamentonuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamentoNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
