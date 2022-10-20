d80f71a2914ec70c82a0fc260fe2a90d8dff4414
Alrocha2301 <alexandre-apr@hotmail.com>
2022-10-20 09:32:10 -0300

    testes 2

--- src/main/java/com/alexandre/estacionamentonuvem/services/ParkingService.java
+++ src/main/java/com/alexandre/estacionamentonuvem/services/ParkingService.java
@@ -20,11 +20,7 @@ public class ParkingService {
     static {
         var id = getUUID();
         Parking parking1 = new Parking(getUUID(),"BNJ-9123", "SP", "Corsa", "Prata");
-        Parking parking2 = new Parking(getUUID(),"DDX-1842", "SP", "Vectra", "Prata");
-        Parking parking3 = new Parking(getUUID(), "EDY-3703", "SP", "Fiesta", "Prata");
         parkingMap.put(parking1.getId(), parking1);
-        parkingMap.put(parking2.getId(), parking2);
-        parkingMap.put(parking3.getId(), parking3);
     }
 
     public List<Parking> findAll() {
--- src/test/java/com/alexandre/estacionamentonuvem/controllers/ParkingControllerIT.java
+++ src/test/java/com/alexandre/estacionamentonuvem/controllers/ParkingControllerIT.java
@@ -1,12 +1,16 @@
 package com.alexandre.estacionamentonuvem.controllers;
 
+import com.alexandre.estacionamentonuvem.controllers.dto.ParkingDTO;
+import com.alexandre.estacionamentonuvem.services.ParkingService;
 import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ParkingControllerIT {
@@ -16,17 +20,36 @@ class ParkingControllerIT {
 
     @BeforeEach
     public void setupTest() {
-        System.out.println(randomPort);
-        RestAssured.port = randomPort;
+         RestAssured.port = randomPort;
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenCheckResult() {
+        RestAssured.given()
+                .when()
+                .get("/v1/parking")
+                .then()
+                .body("license[0]", Matchers.equalTo("BNJ-9123"))
+                .statusCode(HttpStatus.OK.value());
     }
 
     @Test
-    void create() {
-    }
+    void whenCreateThenCheckIsCreated() {
 
-    //implementar testes findAll
+        var dto = new ParkingDTO();
+        dto.setColor("Branco");
+        dto.setLicense("BBB-2023");
+        dto.setModel("Punto");
+        dto.setState("SP");
+
+        RestAssured.given()
+                .when()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(dto)
+                .post("/v1/parking")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("license", Matchers.equalTo("BBB-2023"))
+                .body("color", Matchers.equalTo("Branco"));
+    }
 }
